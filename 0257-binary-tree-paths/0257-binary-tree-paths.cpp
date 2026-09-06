daa4d0110ad6aa250fff1c9cb6348ce0e8f8a62c
Haritika2005 <harryorophile@gmail.com>
2026-09-06 13:46:40 +0530

    Time: 0 ms (100%), Space: 17.8 MB (35.88%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+    void getpath(TreeNode* root, string path, vector<string>& ans) {
+
+        if(root == NULL)
+            return;
+
+        path += to_string(root->val);
+
+        // Leaf node
+        if(root->left == NULL && root->right == NULL) {
+            ans.push_back(path);
+            return;
+        }
+
+        path += "->";
+
+        getpath(root->left, path, ans);
+        getpath(root->right, path, ans);
+    }
+
+    vector<string> binaryTreePaths(TreeNode* root) {
+
+        vector<string> ans;
+
+        if(root == NULL)
+            return ans;
+
+        getpath(root, "", ans);
+
+        return ans;
+    }
+};
